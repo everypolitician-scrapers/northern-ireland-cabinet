df02941910fc6769b6a280a1cf8a92a3ed344886
Tony Bowden <tony@tmtm.com>
2019-02-27 12:26:58 +0000

    Initial scraper

---
+++ scraper.rb
@@ -0,0 +1,8 @@
+#!/bin/env ruby
+# frozen_string_literal: true
+
+require 'scraped'
+require 'scraperwiki'
+require_relative 'lib/cabinet'
+
+Scraped::Scraper.new('Q3272410' => CabinetScraper).store(:memberships, index: %i[position_id])
